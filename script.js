9848f8528864e91dffdf6225ef09103c8eafeaae
lilian <lilianprofissional29@gmail.com>
2021-08-18 10:12:22 -0300

    confirmação inicial

---
+++ script.js
@@ -0,0 +1,22 @@
+const bEntrar = document.getElementById('button')
+
+bEntrar.addEventListener('Click', (event) => {
+    event.preventDefault()
+    
+    const email = document.getElementById('email')
+    const senha = document.getElementById('senha')
+    const nome = document.getElementById('nome')
+    const tel = document.getElementById('tel')
+    const senhaNova = document.getElementById('senhaNova')
+    const check1 = document.getElementById('check1')
+    const recSenha = document.getElementById('recSenha')
+    const confSenha = document.getElementById('confSenha2')
+
+    if (email.value == ""){
+    
+    }
+
+    if (senha.value == ""){
+        
+    }
+})
